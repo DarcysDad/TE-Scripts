c635f02e0cc811eeb409d1991b3da8d1a2de114e
DarcysDad <46019777+DarcysDad@users.noreply.github.com>
2022-01-21 11:29:56 +0000

    Add files via upload

--- Create the Explicit Measure for all selected Columns.cs
+++ Create the Explicit Measure for all selected Columns.cs
@@ -1,13 +1,10 @@
 /* Create an explicit measure as a SUM of the Implicit Column
-
 VERSION: 0.1.0
 AUTHOR: SDUFFY
 ------------------------
-
 DESCRIPTION: Creates a SUM measure for every currently selected column.  The name of the new AddMeasure
 Will be the column name minus the first character (It is anticipated that the Column name Will
 be prefixed with a single special character to differentiate it form the measure name)
-
 INSTRUCTION: C# Script to be executed in Tabular Editor > Adavanced Scripting window
 --------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------*/
@@ -17,7 +14,11 @@ foreach(var c in Selected.Columns)
     //Setup a string to be that of the Column name minus the first character
     string ImplicitName = c.Name;
     string ExplicitName = ImplicitName.Substring(1);
+    string LeftChar = ".";
+    string Compare = ImplicitName.Left(1);
 
+    if (String.Equals(LeftChar,Compare))
+{
     var newMeasure = c.Table.AddMeasure(
         ExplicitName,                          // Name
         "SUM(" + c.DaxObjectFullName + ")",    // DAX expression
@@ -32,4 +33,5 @@ foreach(var c in Selected.Columns)
 
     // Hide the base column:
     c.IsHidden = true;
+}
 }
